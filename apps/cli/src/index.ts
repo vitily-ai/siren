01d93a5574da9b120bd29294e80ad04355db3e80
ganon <ganonballgame@gmail.com>
2026-01-13 20:41:25 -0500

    cli linking

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import { version } from '@siren/core';
 
 console.log(`Siren CLI v${version}`);
--- apps/cli/tsup.config.ts
+++ apps/cli/tsup.config.ts
@@ -6,4 +6,7 @@ export default defineConfig({
   dts: true,
   clean: true,
   shims: true,
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
 });
