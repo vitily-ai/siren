50bd32f08a7512dc1c4b0fac31746fe6ac1e6f07
ganon <ganonballgame@gmail.com>
2026-05-11 02:16:35 -0400

    core: remove export surface and introduce mutative patch api

--- packages/core/src/ir/assembly.ts
+++ packages/core/src/ir/assembly.ts
@@ -29,6 +29,51 @@ export class SirenBuilder {
     return this.documentsSnapshot;
   }
 
+  patch(fn: (docs: readonly SirenDocument[]) => readonly SirenDocument[]): SirenBuilder {
+    return SirenBuilder.fromDocuments(fn(this.documentsSnapshot));
+  }
+
+  withDocument(doc: SirenDocument): SirenBuilder {
+    return this.patch((docs) => {
+      return [...docs, doc];
+    });
+  }
+
+  patchDocument(documentId: string, fn: (doc: SirenDocument) => SirenDocument): SirenBuilder {
+    return this.patch((docs) => docs.map((d) => (d.id === documentId ? fn(d) : d)));
+  }
+
+  withResource(resource: Resource, documentId = 'misc'): SirenBuilder {
+    const existingDocument = this.documentsSnapshot.find((d) => d.id === documentId);
+    if (existingDocument === undefined) {
+      return this.withDocument({
+        id: documentId,
+        resources: [resource],
+        directive: { implicitMilestone: false },
+      });
+    }
+
+    return this.patchDocument(documentId, (doc) => ({
+      ...doc,
+      resources: [...doc.resources, resource],
+    }));
+  }
+
+  patchResource(resourceId: string, fn: (res: Resource) => Resource): SirenBuilder {
+    return this.patch((docs) =>
+      docs.map((doc) => {
+        if (!doc.resources.some((r) => r.id === resourceId)) {
+          return doc;
+        }
+
+        return {
+          ...doc,
+          resources: doc.resources.map((r) => (r.id === resourceId ? fn(r) : r)),
+        };
+      }),
+    );
+  }
+
   build(): SirenProject {
     return SirenProject[IR_CONTEXT_FACTORY](this.documentsSnapshot);
   }
